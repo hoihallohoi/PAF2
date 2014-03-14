bd00b530d0bd25457bbc8279e1c99d9944e3c31e
joel troost <joeltroost@hotmail.com>
2014-03-14 14:37:23 +0100

    pattern update

--- src/domain/Pattern.java
+++ src/domain/Pattern.java
@@ -1,12 +1,17 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class Pattern {
-	private String patternName, problem, solution;
+	private String patternName, problem, solution,consequence;
+	private ArrayList<String> alternatives = new ArrayList<String>();
 	
-	public Pattern(String name, String prob, String sol){
+	public Pattern(String name, String prob, String sol, String con, ArrayList<String> alt){
 		name = patternName;
 		prob = problem;
 		sol = solution;
+		con = consequence;
+		alt = alternatives;
 	}
 
 	public String getPatternName() {
@@ -15,6 +20,22 @@ public class Pattern {
 
 	public void setPatternName(String patternName) {
 		this.patternName = patternName;
+	}
+
+	public String getConsequence() {
+		return consequence;
+	}
+
+	public void setConsequence(String consequence) {
+		this.consequence = consequence;
+	}
+
+	public ArrayList<String> getAlternatives() {
+		return alternatives;
+	}
+
+	public void setAlternatives(ArrayList<String> alternatives) {
+		this.alternatives = alternatives;
 	}
 
 	public String getProblem() {
